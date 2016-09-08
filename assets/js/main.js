d8abd46a8b5e901ddd7c9533afef4e46dea31ce6
snyoonoh <snyoonoh@gmail.com>
2016-09-08 00:27:22 -0700

    disabled hover effects for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,23 +8,7 @@ function myFunction() {
     }
 }
 
-var touch = window.ontouchstart
-            || (navigator.MaxTouchPoints > 0)
-            || (navigator.msMaxTouchPoints > 0);
-
-if (touch) { // remove all :hover stylesheets
-    try { // prevent exception on browsers not supporting DOM styleSheets properly
-        for (var si in document.styleSheets) {
-            var styleSheet = document.styleSheets[si];
-            if (!styleSheet.rules) continue;
-
-            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                if (!styleSheet.rules[ri].selectorText) continue;
-
-                if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                    styleSheet.deleteRule(ri);
-                }
-            }
-        }
-    } catch (ex) {}
+var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+if (!touchsupport){ // browser doesn't support touch
+    document.documentElement.className += " non-touch"
 }
